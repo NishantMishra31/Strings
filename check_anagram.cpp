0ebb2719760ec79360b3ac72f9fb4cb329a41718
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-27 01:05:27 +0530

    Added Files and code changes

--- check_anagram.cpp
+++ check_anagram.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 bool isAnagram(string s, string t) {
     if (s.size() != t.size()) return false;
-    int freq[26] = {0};
-    for (char c : s) freq[c - 'a']++;
-    for (char c : t) freq[c - 'a']--;
-    for (int i = 0; i < 26 ; ++i) {
-        if (freq[i] != 0) return false;
-    }
-    return true;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    return s == t;
 }
 
 int main(){
