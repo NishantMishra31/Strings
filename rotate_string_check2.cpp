b8fc88bcc86417214fd8ebb3ce4c281bc2813a12
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-27 00:38:19 +0530

    Added Files and code changes

--- rotate_string_check2.cpp
+++ rotate_string_check2.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 bool rotateString(string s, string goal) {
     if (s.length() != goal.length()) return false;
-    int n = s.length();
-    for (int i = 0; i < n; ++i) {
-        char firstChar = s[0];
-        s = s.substr(1) + firstChar;
-        if (s == goal) return true;
-    }
+    string concat = s + s;
+    return concat.find(goal) != -1;
     return false;
 }
 
