32083b308a85556ca3144e354b5672948acff60f
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-26 13:18:37 +0530

    Added Files and code changes

--- remove_parenthesis.cpp
+++ remove_parenthesis.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 string removeOuterParentheses(string s) {
     stack<char> stk;
-    string result;
+    string ans;
     int start = 0;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == '(') stk.push('(');
         else stk.pop();
         if (stk.empty()) {
-            result += s.substr(start + 1, i - start - 1);
+            ans += s.substr(start + 1, i - start - 1);
             start = i + 1;
         }
     }
-    return result;
+    return ans;
 }
-
 
 int main(){
     string s;
