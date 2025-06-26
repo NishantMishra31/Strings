1f1880c67f07ad6a5cd71f3d0f102703ba1b3bfc
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-26 18:44:50 +0530

    Added Files and code changes

--- longest_common_prefix2.cpp
+++ longest_common_prefix2.cpp
@@ -2,25 +2,17 @@
 using namespace std;
 
 string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-
-        // Step 1: Sort the strings
-        sort(strs.begin(), strs.end());
-
-        string first = strs[0];
-        string last = strs[strs.size() - 1];
-        string prefix = "";
-
-        // Step 2: Compare first and last string character-by-character
-        for (int i = 0; i < min(first.size(), last.size()); ++i) {
-            if (first[i] == last[i])
-                prefix += first[i];
-            else
-                break;
-        }
-
-        return prefix;
+    if (strs.empty()) return "";
+    sort(strs.begin(), strs.end());
+    string first = strs[0];
+    string last = strs[strs.size() - 1];
+    string prefix = "";
+    for (int i = 0; i < min(first.size(), last.size()); ++i){
+        if (first[i] == last[i]) prefix += first[i];
+        else break;
     }
+    return prefix;
+}
 
 int main() {
     int n;
